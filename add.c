679ef859d8b3b6c3da979c927942f799636ba428
Linnea <148188744+NEAAA4@users.noreply.github.com>
2024-09-18 15:24:27 +0200

    Update add.c

--- add.c
+++ add.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main (int argc, char *argv[]){
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
 
-  return 0;
+    int sum = num1 + num2;
+
+    return sum;
 }
