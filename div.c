3c7b343de3eac1579708898464631dc4f5ebe183
Linnea <148188744+NEAAA4@users.noreply.github.com>
2024-09-18 15:24:55 +0200

    Update div.c

--- div.c
+++ div.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
 
-  return 0;
+int main(int argc, char *argv[]){
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+
+    if (num1 == str || num2 == str){
+        return 0;
+    }
+    if (num2 == 0){
+        printf("DIV ZERO NOT ALLOWED!\n");
+        return 0;
+    }
+
+    int sum = num1 / num2;
+    return sum;
 }
